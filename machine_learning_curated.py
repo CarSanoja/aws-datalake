30d9dfb301853af66a4071f510625f81f28ee68f
Carlos Sanoja <34045195+CarSanoja@users.noreply.github.com>
2024-09-01 21:14:59 -0400

    Update machine_learning_curated.py

--- machine_learning_curated.py
+++ machine_learning_curated.py
@@ -18,28 +18,11 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# Script generated for node step trainer trusted
-steptrainertrusted_node1725233075484 = glueContext.create_dynamic_frame.from_catalog(database="stedi_database", table_name="step_trainer_trusted", transformation_ctx="steptrainertrusted_node1725233075484")
-
 # Script generated for node acce trusted
 accetrusted_node1725233076077 = glueContext.create_dynamic_frame.from_catalog(database="stedi_database", table_name="accelerometer_trusted_san", transformation_ctx="accetrusted_node1725233076077")
 
-# Script generated for node customer curated
-customercurated_node1725235296798 = glueContext.create_dynamic_frame.from_catalog(database="stedi_database", table_name="customer_trusted_san", transformation_ctx="customercurated_node1725235296798")
-
-# Script generated for node SQL Query
-SqlQuery1 = '''
-SELECT
-    ac.*,
-    cc.*
-FROM
-    accelerometer_trusted ac
-JOIN
-    customers_curated cc
-ON
-    ac.user = cc.email
-'''
-SQLQuery_node1725235600865 = sparkSqlQuery(glueContext, query = SqlQuery1, mapping = {"accelerometer_trusted":accetrusted_node1725233076077, "customers_curated":customercurated_node1725235296798}, transformation_ctx = "SQLQuery_node1725235600865")
+# Script generated for node step trainer trusted
+steptrainertrusted_node1725233075484 = glueContext.create_dynamic_frame.from_catalog(database="stedi_database", table_name="step_trainer_trusted", transformation_ctx="steptrainertrusted_node1725233075484")
 
 # Script generated for node SQL Query
 SqlQuery0 = '''
@@ -47,27 +30,43 @@ SELECT
     st.serialNumber AS step_trainer_serialNumber,
     st.sensorReadingTime AS step_trainer_time,
     st.distanceFromObject,
-    ac_curated.timeStamp AS accelerometer_time,
-    ac_curated.x AS accelerometer_x,
-    ac_curated.y AS accelerometer_y,
-    ac_curated.z AS accelerometer_z
+    ac.timeStamp AS accelerometer_time,
+    ac.x AS accelerometer_x,
+    ac.y AS accelerometer_y,
+    ac.z AS accelerometer_z
 FROM
-    step_trainer_trusted st
-JOIN ac_curated
+    (SELECT 
+        sensorReadingTime, 
+        serialNumber, 
+        distanceFromObject 
+     FROM 
+        step_trainer_trusted 
+     WHERE 
+        sensorReadingTime IS NOT NULL) st
+JOIN
+    (SELECT 
+        timeStamp, 
+        user, 
+        x, 
+        y, 
+        z 
+     FROM 
+        accelerometer_trusted 
+     WHERE 
+        timeStamp IS NOT NULL) ac
 ON
-    st.serialNumber = ac_curated.serialNumber
-AND
-    st.sensorReadingTime = ac_curated.timeStamp
-GROUP BY
-    st.serialNumber,
-    st.sensorReadingTime,
-    st.distanceFromObject,
-    ac_curated.timeStamp,
-    ac_curated.x,
-    ac_curated.y,
-    ac_curated.z;
+    st.sensorReadingTime = ac.timeStamp
+WHERE
+    ac.user IN (SELECT email FROM customers_curated)
+;
 '''
-SQLQuery_node1725233120115 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"step_trainer_trusted":steptrainertrusted_node1725233075484, "ac_curated":SQLQuery_node1725235600865}, transformation_ctx = "SQLQuery_node1725233120115")
+SQLQuery_node1725233120115 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"step_trainer_trusted":steptrainertrusted_node1725233075484, "accelerometer_trusted":accetrusted_node1725233076077}, transformation_ctx = "SQLQuery_node1725233120115")
+
+# Script generated for node SQL Query
+SqlQuery1 = '''
+SELECT 'Curated', 'Machine Learning', COUNT(*) FROM myDataSource;
+'''
+SQLQuery_node1725238257579 = sparkSqlQuery(glueContext, query = SqlQuery1, mapping = {"myDataSource":SQLQuery_node1725233120115}, transformation_ctx = "SQLQuery_node1725238257579")
 
 # Script generated for node Amazon S3
 AmazonS3_node1725234272532 = glueContext.getSink(path="s3://machine-learning-curated-san/data/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="AmazonS3_node1725234272532")
